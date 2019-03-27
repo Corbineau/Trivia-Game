3b42191b5b8338fb0a48c732804de0691c989507
Rabbit <Corbineau@users.noreply.github.com>
2019-03-27 17:05:51 -0400

    actually included the link to the js

--- resources/js/game.js
+++ resources/js/game.js
@@ -1,17 +1,45 @@
-function Question(q, a, b, c, d, answer, rightModal, wrongModal) {
-    q = this.q;
-    a = this.a;
-    b = this.b;
-    c = this.c;
-    d = this.d;
-    answer = this.answer; //this is the letter of the correct answer
-}
+const questions = [
+    {
+        q: "In Latin eduction, The <i>Trivia<i> are the three foundational liberal arts:",   
+        a: "reading, writing, and arithmetic",
+        b: "history, literature, and philosophy",
+        c: "grammar, logic, and rhetoric",
+        d: "blood, love, and rhetoric",
+        answer: "c",
+    },
+    {
+        q: "The board game <i>Trivial Pursuit</i> was first released in:",
+        a: "1982",
+        b: "1973",
+        c: "1979",
+        d: "1984",
+        answer: "a",
 
-const trivia = [q1 = new Question("In Latin eduction, The <i>Trivia<i> are the three foundational liberal arts:", "reading, writing, and arithmetic", "history, literature, and philosophy", "grammar, logic, and rhetoric", "blood, love, and rhetoric", c), q2 = new Question("The board game <i>Trivial Pursuit</i> was first released in:", "1982", "1973", "1979", "1984", a), q3 = new Question("The word <i>triviae</i> originally meant _________, which ultimately came to mean 'a public place', and finally, 'commonplace'.", "three lives", "three roads", "third water", "well-travelled roads", b), q4 = new Question("The word 'trivia' came to mean 'trite, unimportant, commonplace, banal' in the ____, largely due to the works of ________", "1300's, Chaucer", "1800's, Logan Pearsall Smith", "1600's, Shakespere", "1700's, Jonathan Swift", c), q4 = new Question("Fred L Worth, author of the Trivia Encyclopedia, deliberately placed misinformation in his books to catch anyone who attempted to violate his copyright. One of these incorrect answers was used in the original Trivial Pursuit board game, 'what was Columbo's first name?' The wrong answer was ______; Columbo's first name was actually _______", "George, Columbo", "Frank, nonexistant", "Frances, Lieutenant", "Philip, Frank (never actually stated)", d), q5 = new Question("Fred L Worth sued <i>Trivial Pursuit</i> for 300 Million dollars for copyright infringment. Trivial Pursuit successfully defeated the suit, arguing that:", "Fred L. Worth actually plagarized someone else.", "You can't copyright facts.", "The Copyright Trap is an invalid tactic, like entrapment.", "A Board Game is protected under Fair Use laws.", b), q6 = new Question("A contemporary slang synonym for trivial is:", "bougie", "ridic", "banjie", "triflin'", d)];
+    }
+]
+
+
+function Question({ q, a, b, c, d, answer }, rightModal, wrongModal) {
+    this.q = q;
+    this.a = a;
+    this.b = b;
+    this.c = c;
+    this.d = d;
+    this.answer = answer; //this is the letter of the correct answer
+}
+const triviaQs = [];
+questions.forEach(function(question) {
+    const triviaQuestion = new Question(question);
+    triviaQs.push(triviaQuestion);
+});
+
+console.log(triviaQs);
+
+ // [q1 = new Question("In Latin eduction, The <i>Trivia<i> are the three foundational liberal arts:", "reading, writing, and arithmetic", "history, literature, and philosophy", "grammar, logic, and rhetoric", "blood, love, and rhetoric", c), q2 = new Question("The board game <i>Trivial Pursuit</i> was first released in:", "1982", "1973", "1979", "1984", a), q3 = new Question("The word <i>triviae</i> originally meant _________, which ultimately came to mean 'a public place', and finally, 'commonplace'.", "three lives", "three roads", "third water", "well-travelled roads", b), q4 = new Question("The word 'trivia' came to mean 'trite, unimportant, commonplace, banal' in the ____, largely due to the works of ________", "1300's, Chaucer", "1800's, Logan Pearsall Smith", "1600's, Shakespere", "1700's, Jonathan Swift", c), q4 = new Question("Fred L Worth, author of the Trivia Encyclopedia, deliberately placed misinformation in his books to catch anyone who attempted to violate his copyright. One of these incorrect answers was used in the original Trivial Pursuit board game, 'what was Columbo's first name?' The wrong answer was ______; Columbo's first name was actually _______", "George, Columbo", "Frank, nonexistant", "Frances, Lieutenant", "Philip, Frank (never actually stated)", d), q5 = new Question("Fred L Worth sued <i>Trivial Pursuit</i> for 300 Million dollars for copyright infringment. Trivial Pursuit successfully defeated the suit, arguing that:", "Fred L. Worth actually plagarized someone else.", "You can't copyright facts.", "The Copyright Trap is an invalid tactic, like entrapment.", "A Board Game is protected under Fair Use laws.", b), q6 = new Question("A contemporary slang synonym for trivial is:", "bougie", "ridic", "banjie", "triflin'", d)];
 
 //set up the timing function
 
-let trivia = {
+let triviaGame = {
 gameOn: false,
 isThinking: false,
 loadTimer: setTimeout(gameStart, 3000),
@@ -76,7 +104,7 @@ gameInit: function() {
 
 $(document).ready( function() {
 
-trivia.loadTimer();
+triviaGame.loadTimer();
 
 });
 
