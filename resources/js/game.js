f06cee4abda23ce945f1b5d5bdf2adb1671a3388
Rabbit <Corbineau@users.noreply.github.com>
2019-03-28 01:48:53 -0400

    added all the questions, started on some CSS

--- resources/js/game.js
+++ resources/js/game.js
@@ -1,3 +1,4 @@
+let triviaQs = [];
 const questions = [
     {
         q: "In Latin eduction, The <i>Trivia<i> are the three foundational liberal arts:",   
@@ -15,6 +16,71 @@ const questions = [
         d: "1984",
         answer: "a",
 
+    },
+    {
+        q: "The word <i>triviae</i> originally meant _________, which ultimately came to mean 'a public place', and finally, 'commonplace'.",
+        a: "three lives",
+        b: "three roads",
+        c: "public roads",
+        d: "water of life",
+        answer: "b",
+    },
+    {
+        q: "The word 'trivia' came to mean 'trite, unimportant, commonplace, banal' in the ____, largely due to the works of ________",
+        a: "1300's, Chaucer",
+        b: "1800's, Logan Pearsall Smith",
+        c: "1600's, Shakespere",
+        d: "1700's, Jonathan Swift",
+        answer: "c",
+    },
+    {
+        q: "Fred L Worth, author of the Trivia Encyclopedia, deliberately placed misinformation in his books to catch anyone who attempted to violate his copyright. One of these incorrect answers was used in the original Trivial Pursuit board game, 'what was Columbo's first name?' The wrong answer was ______; Columbo's first name was actually _______",
+        a: "George, Columbo", 
+        b: "Frank, nonexistant", 
+        c: "Frances, Lieutenant", 
+        d: "Philip, Frank (never actually stated)", 
+        answer: "d",
+    },
+    {
+        q: "Fred L Worth sued <i>Trivial Pursuit</i> for 300 Million dollars for copyright infringment. Trivial Pursuit successfully defeated the suit, arguing that:", 
+        a: "Fred L. Worth actually plagarized someone else.", 
+        b: "You can't copyright facts.", 
+        c: "The Copyright Trap is an invalid tactic, like entrapment.", 
+        d: "A Board Game is protected under Fair Use laws.", 
+        answer: "b",
+    },
+    {
+        q: "A contemporary slang synonym for trivial is:", 
+        a: "bougie", 
+        b: "ridic", 
+        c: "banjie", 
+        d: "triflin'",
+        answer: "d",
+    },
+    {
+        q: "The first known labeling of trivia as a casual parlor game was in a February 5th, 1965 column published in which newspaper?",
+        a: "The Columbia Daily Spectator",
+        b: "The Sacramento News and Review",
+        c: "The New York Times",
+        d: "The Portland Press Herald",
+        Answer: "a", 
+    },
+    {
+        q: "The two longest ongoing trivia contests in the world are the Great Midwest Trivia Contest at Lawrence University and the Williams Trivia Contest, both of which started in the spring of which year?",
+        a: "1959",
+        b: "1982",
+        c: "1979",
+        d: "1966",
+        answer: "d",
+    },
+    {
+        q: "Which televised trivia competition was the framing device for a popular 2008 movie set in Mumbai, India?",
+        a: "Jeopardy",
+        b: "Who Wants To Be a Millionaire?",
+        c: "The $100,000 Pyramid",
+        d: "Bollywood Squares",
+        Answer: "b",
+        
     }
 ]
 
@@ -27,7 +93,6 @@ function Question({ q, a, b, c, d, answer }, rightModal, wrongModal) {
     this.d = d;
     this.answer = answer; //this is the letter of the correct answer
 }
-const triviaQs = [];
 questions.forEach(function(question) {
     const triviaQuestion = new Question(question);
     triviaQs.push(triviaQuestion);
@@ -35,15 +100,13 @@ questions.forEach(function(question) {
 
 console.log(triviaQs);
 
- // [q1 = new Question("In Latin eduction, The <i>Trivia<i> are the three foundational liberal arts:", "reading, writing, and arithmetic", "history, literature, and philosophy", "grammar, logic, and rhetoric", "blood, love, and rhetoric", c), q2 = new Question("The board game <i>Trivial Pursuit</i> was first released in:", "1982", "1973", "1979", "1984", a), q3 = new Question("The word <i>triviae</i> originally meant _________, which ultimately came to mean 'a public place', and finally, 'commonplace'.", "three lives", "three roads", "third water", "well-travelled roads", b), q4 = new Question("The word 'trivia' came to mean 'trite, unimportant, commonplace, banal' in the ____, largely due to the works of ________", "1300's, Chaucer", "1800's, Logan Pearsall Smith", "1600's, Shakespere", "1700's, Jonathan Swift", c), q4 = new Question("Fred L Worth, author of the Trivia Encyclopedia, deliberately placed misinformation in his books to catch anyone who attempted to violate his copyright. One of these incorrect answers was used in the original Trivial Pursuit board game, 'what was Columbo's first name?' The wrong answer was ______; Columbo's first name was actually _______", "George, Columbo", "Frank, nonexistant", "Frances, Lieutenant", "Philip, Frank (never actually stated)", d), q5 = new Question("Fred L Worth sued <i>Trivial Pursuit</i> for 300 Million dollars for copyright infringment. Trivial Pursuit successfully defeated the suit, arguing that:", "Fred L. Worth actually plagarized someone else.", "You can't copyright facts.", "The Copyright Trap is an invalid tactic, like entrapment.", "A Board Game is protected under Fair Use laws.", b), q6 = new Question("A contemporary slang synonym for trivial is:", "bougie", "ridic", "banjie", "triflin'", d)];
-
 //set up the timing function
 
 let triviaGame = {
 gameOn: false,
 isThinking: false,
-loadTimer: setTimeout(gameStart, 3000),
-thinkTimer: setTimeout(checkAnswer, 15000),
+loadTimer: setTimeout(this.gameStart, 3000),
+thinkTimer: setTimeout(this.checkAnswer, 15000),
 setInterval: (this.count, 1000),
 count: 0,
 numRight: 0,
@@ -53,6 +116,7 @@ numWrong: 0,
 gameStart: function() {
     this.gameOn = true;
     this.questionLoop();
+    console.log(this.gameOn);
 
 },
 // Iterate through questions array while gameOn is true. (possibly do this randomly?)
@@ -104,7 +168,7 @@ gameInit: function() {
 
 $(document).ready( function() {
 
-triviaGame.loadTimer();
+//triviaGame.loadTimer();
 
 });
 
